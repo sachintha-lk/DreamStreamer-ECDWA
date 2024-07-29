5e5203e4af3ff4c048405a79a1875252736c444f
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2024-07-29 14:10:03 +0530

    add login pg ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Button } from "@/components/ui/button"
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { ThemeProvider } from "@/components/theme-provider"
 
+import Login from "./pages/Login"
+import { DarkLightModeToggle } from "./components/dark-light-mode-toggle"
+ 
 
 function App() {
 
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="absolute top-3 right-3 z-10">
+          <DarkLightModeToggle/>
+      </div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="login" element={<Login />} />
+        
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </ThemeProvider>
   )
 }
 
